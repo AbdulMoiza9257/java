9f076c19c7cd76e5f093194624007ce1fb2ac53e
AbdulMoiza9257 <abdulmoiza9257@gmail.com>
2023-12-01 16:24:06 -0800

    done

---
+++ app.js
@@ -0,0 +1,9 @@
+var a = +prompt("Pys");
+var b = +prompt("Math");
+var c = +prompt("English");
+var d = +prompt("Bio");
+var e = +prompt("Urdu");
+
+var f = 500;
+var percentage = ((a+b+c+d+e)/f)*100
+alert(percentage)
